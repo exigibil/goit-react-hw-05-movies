ed8e28556f2764f6ca5b253d2c2d72904b8a4090
exigibil <adrian.topa04@gmail.com>
2024-05-27 21:18:26 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './ContentSite/Pages/Home';
 import Movies from './ContentSite/Pages/Movies';
 import MoviesDetails from './ContentSite/Pages/DetailsMovie';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/movies/:movieId" element={<MoviesDetails />} />
-    </Routes>
+    
+    <Router basename="/goit-react-hw-05-movies">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MoviesDetails />} />
+      </Routes>
+  </Router>
+    
   );
 };
