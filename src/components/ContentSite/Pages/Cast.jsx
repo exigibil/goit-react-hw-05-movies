3d8bfc0de2a2289fcb16f01352872aad5454b69b
exigibil <adrian.topa04@gmail.com>
2024-05-31 22:51:53 +0300

    update cast

--- src/components/ContentSite/Pages/Cast.jsx
+++ src/components/ContentSite/Pages/Cast.jsx
@@ -1,18 +1,24 @@
+import styles from "../MovieDetails/MovieDetails.module.css"
+
+
 function Cast({ cast }) {
     console.log("Cast component received cast data:", cast);
   
     return (
       <>
         {cast && (
-          <div>
+          <div className={styles.castContainer}>
             <h2>Cast</h2>
-            <ul>
+            <ul className={styles.castlistContainer} >
               {cast.map(actor => (
-                <li key={actor.id}>{actor.name}
-                <img
-                src={`https://image.tmdb.org/t/p/w500/${actor.poster_path}`}
+                <li className={styles.castList}  key={actor.id}>
+              <div>{actor.name}</div>
+               <div>
+               <img
+                src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`}
                 alt={actor.name}
               />
+                </div> 
                 </li>
               ))}
             </ul>
