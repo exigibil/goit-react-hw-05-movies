d10fab28210193ff4bf168708afc43549b4bd4f2
exigibil <adrian.topa04@gmail.com>
2024-06-02 21:24:51 +0300

    commit

--- src/components/API/apikey.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = "9899002b9b51fd4dfe6aea31ec85120f";
-export const baseURL = "https://api.themoviedb.org/3"
--- src/components/ContentSite/Pages/NotFoundError.jsx
+++
@@ -1,7 +0,0 @@
-function NotFoundError() {
-    return (
-      <h1>404 Not Found - The page does not exist</h1>
-    );
-  }
-  
-  export default NotFoundError;
